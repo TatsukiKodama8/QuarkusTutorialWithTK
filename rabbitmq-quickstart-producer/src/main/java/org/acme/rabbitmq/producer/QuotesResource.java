dcc47bd115c0954671371f94bd21ea91b40cecf0
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-02-21 08:44:02 +0900

    [add] rabbitmq

---
+++ rabbitmq-quickstart-producer/src/main/java/org/acme/rabbitmq/producer/QuotesResource.java
@@ -0,0 +1,34 @@
+package org.acme.rabbitmq.producer;
+
+import java.util.UUID;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import org.acme.rabbitmq.model.Quote;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import io.smallrye.mutiny.Multi;
+
+@Path("/quotes")
+public class QuotesResource {
+
+    @Channel("quote-requests") Emitter<String> quoteRequestEmitter; 
+
+    /**
+     * Endpoint to generate a new quote request id and send it to "quote-requests" channel (which
+     * maps to the "quote-requests" RabbitMQ exchange) using the emitter.
+     */
+    @POST
+    @Path("/request")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String createRequest() {
+        UUID uuid = UUID.randomUUID();
+        quoteRequestEmitter.send(uuid.toString()); 
+        return uuid.toString();
+    }
+}
